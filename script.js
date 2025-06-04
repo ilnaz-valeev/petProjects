5165718e4401104cc6eefac175b24a07b8432794
ilnaz valeev <alexsanderkyznecov@mail.ru>
2025-06-04 23:56:51 +0500

    добавил то что цифры показывались сколько оставлось задачь

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const taskList = document.querySelector(".taskList");
 const inputTask = document.querySelector(".inputTask");
 const clearButton = document.querySelector(".clearButton"); // Переименовано для ясности
+const countingTask = document.querySelector(".countingTask");
+const logo = document.querySelector(".logo");
 
 // Добавление задачи
 inputTask.addEventListener("keydown", (e) => {
   if (e.key === "Enter" && inputTask.value.trim() !== "") {
     e.preventDefault();
     addTask(inputTask.value.trim());
+    updateTaskCount();
     inputTask.value = "";
   }
 });
@@ -27,11 +30,13 @@ function addTask(taskText) {
   taskList.insertAdjacentHTML("beforeend", taskHTML);
 }
 
+
 // Удаление задач
 taskList.addEventListener("click", (e) => {
   const deleteBtn = e.target.closest(".cross-btn");
   if (deleteBtn) {
     deleteBtn.closest("li").remove();
+    updateTaskCount();
     return;
   }
 
@@ -48,5 +53,22 @@ clearButton.addEventListener("click", () => {
   const completedTasks = document.querySelectorAll(".task.checked"); // Правильное имя
   completedTasks.forEach((task) => {
     task.remove();
+    updateTaskCount();
   });
+});
+
+
+function updateTaskCount() {
+    const tasks = document.querySelectorAll(".task")
+    countingTask.textContent = tasks.length;
+}
+
+
+logo.addEventListener("click",(e)=>{
+    
+    if (logo == ligth) {
+      body = ligth;
+    } else {
+      body = dark;
+    }
 });
