e240d77de7d3981bd5636b18ac43a9b5fede8d67
ilnaz valeev <alexsanderkyznecov@mail.ru>
2025-06-14 23:54:49 +0500

    Начинаю делать js

---
+++ financial_tracker/trackerScript.js
@@ -0,0 +1,17 @@
+const addButton = document.querySelector(".btn");
+
+
+addButton.addEventListener('click', (e)=>{
+    if (input.value === '' || 0) {
+        alert("Пожалуйста заполните все поля, чтобы добавить операцию")
+    }else{
+
+    }
+})
+
+function addOperation() {
+    const Operations = 
+    `
+    
+    `
+}
