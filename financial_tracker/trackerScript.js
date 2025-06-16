15ee42cae29af2c9eb38a44e7b909198ed937fa5
ilnaz valeev <alexsanderkyznecov@mail.ru>
2025-06-16 07:55:32 +0500

    Добавил перевод на русский значения категорий тип операций и тд(отображалсиь на английском )

--- financial_tracker/trackerScript.js
+++ financial_tracker/trackerScript.js
@@ -7,9 +7,20 @@ const category = document.querySelector("#category");
 const date = document.querySelector("#date");
 const emptyState = document.querySelector(".empty-state");
 
+// Объекты для перевода значений на русский
+const typeTranslations = {
+  income: "Доход",
+  expense: "Расход",
+};
 
-
-
+const categoryTranslations = {
+  food: "Еда",
+  transport: "Транспорт",
+  housing: "Жилье",
+  entertainment: "Развлечения",
+  salary: "Зарплата",
+  other: "Другое",
+};
 
 addButton.addEventListener("click", (e) => {
   e.preventDefault(); // предотвращаем перезагрузку страницы
@@ -24,24 +35,31 @@ addButton.addEventListener("click", (e) => {
   } else {
     addOperation();
   }
-  
 });
 
 function addOperation() {
   const transactionList = document.querySelector("#transactions-list");
 
+  // Удаляем элемент "empty-state", если транзакции добавлены
   if (transactionList.children.length > 0) {
     emptyState.remove();
   }
-  
+
+  // Перевод значений с использованием словарей
+  const translatedType = typeTranslations[type.value] || type.value;
+  const translatedCategory =
+    categoryTranslations[category.value] || category.value;
+
+  // Добавляем новую операцию в список
   const operation = `
     <li>
         <span>${description.value}</span>
         <span>${amount.value}</span>
-        <span>${type.value}</span>
-        <span>${category.value}</span>
+        <span>${translatedType}</span>
+        <span>${translatedCategory}</span>
         <span>${date.value}</span>
     </li>`;
+
+  // Вставляем операцию в список
   transactionList.innerHTML += operation;
 }
-
